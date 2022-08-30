b07934828d0470ad5a0e4f5f7b5dbb740e5cdbe6
Nathan Lee <worldnumber1pingpongplayer@gmail.com>
2022-08-30 13:52:23 -0700

    local changes

--- server/routes/tourneys.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getTourneys, createTourney, getTourneyInfo } from "../controllers/tourneys.js";
-
-const router = express.Router();
-
-
-
-export default router;
